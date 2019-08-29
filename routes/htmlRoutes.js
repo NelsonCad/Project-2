440435bbda3bf79765e69f538d5dfa44b3bd05fa
NelsonCad <NelsonCad97@gmail.com>
2019-08-29 14:28:20 -0700

    changed handlbars pages, html routes, some api routes, database (still waiting)

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,44 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+module.exports = function (app) {
+
+  // loads splash page
+  app.get("/", function (req, res) {
+    res.send("index");
+  })
+
+  // Load arts page
+  app.get("/art", function (req, res) {
+    db.Piece.findAll()
+      .then(function (piece) {
+        res.render("artPage", {
+          piece: piece
+        });
+      }).catch(err => { throw err });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/artists", function (req, res) {
+    db.Artist.findAll()
+      .then(function (artist) {
+        res.render("artistPage", {
+          artist: artist
+        });
+      }).catch(err => { throw err });
+  });
+
+  // loads the Login page
+  app.get("/login", function (req, res) {
+    res.render("artistlogin");
+  });
+
+  // loads the art submission page
+  app.get("/submit", function (req,res) {
+    res.render("artsubmit");
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
