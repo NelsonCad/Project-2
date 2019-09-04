856af9ac464d16335b85731e4e850a3fab866284
NelsonCad <NelsonCad97@gmail.com>
2019-09-03 19:58:40 -0700

    finished merge conflicts and errors, added basis to profile and failure page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,12 @@
 var db = require("../models");
 
-<<<<<<< HEAD
-module.exports = function (app) {
-=======
 const AWS = require("aws-sdk");
 const multer = require("multer");
->>>>>>> 67c117613bd748e4ba0b8e6fc1ec8691a974078c
 
 // Multer File Middleware
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-<<<<<<< HEAD
-  // Get all art pieces
-  app.get("/api/art", function (req, res) {
-    db.Piece.findAll({}).then(function (pieces) {
-      res.json(pieces);
-    });
-  });
-
-  // Submit a piece of art
-  app.post("/api/art", function (req, res) {
-    db.Piece.create(req.body).then(function (pieces) {
-      res.json(pieces);
-=======
 module.exports = function (app) {
   // Get all examples
   app.get("/api/artists", function (req, res) {
@@ -36,20 +19,18 @@ module.exports = function (app) {
   app.post("/api/post", function (req, res) {
     db.Artist.create(req.body).then(function () {
       res.status(200).end();
->>>>>>> 67c117613bd748e4ba0b8e6fc1ec8691a974078c
     });
   });
   // Create a new artist piece
   app.post("/api/newPiece", upload.single("file"), function (req, res) {
     db.Piece.create(
-      
+
     )
   });
-}
 
-<<<<<<< HEAD
+
   // Update a post
-  app.put("api/art/:id", function (req,res) {
+  app.put("api/art/:id", function (req, res) {
 
   });
 
@@ -79,5 +60,3 @@ module.exports = function (app) {
   });
 
 };
-=======
->>>>>>> 67c117613bd748e4ba0b8e6fc1ec8691a974078c
